e04b26bd9e448296a9780c4e3ef8eb4ffcef7d2d
kai-lee <bbruesch@mail.csuchico.edu>
2013-11-11 23:36:55 -0800

    working on css

---
+++ papp/db/migrate/20131112061225_create_photos.rb
@@ -0,0 +1,13 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+      t.string :user_id
+      t.string :name
+      t.text :description
+      t.text :tags
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
--- photoapp/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Photoapp::Application.config.session_store :cookie_store, key: '_photoapp_session'
