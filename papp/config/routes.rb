deaf22b69e5ad44245e1c3a3e9cd16cde55c815b
kai-lee <bbruesch@mail.csuchico.edu>
2013-11-12 00:27:45 -0800

    still working on search and css

--- papp/config/routes.rb
+++ papp/config/routes.rb
@@ -2,12 +2,13 @@ Papp::Application.routes.draw do
   devise_for :users
   resources :photos
 
+#root :to => redirect('/photos')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+root 'photos#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
